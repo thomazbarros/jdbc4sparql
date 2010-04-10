580c4f1cd99150aba5066a5ceaba4e21fc0a75a9
williamgreenly <williamgreenly@e82af53c-6218-6976-620e-793e16141d9a>
2010-04-10 20:04:54 +0000

    Progress

---
+++ src/org/lexicon/jdbc4sparql/SPARQLSelectResultSetMetaData.java
@@ -0,0 +1,128 @@
+package org.lexicon.jdbc4sparql;
+
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class SPARQLSelectResultSetMetaData implements ResultSetMetaData {
+
+	public SPARQLSelectResultSetMetaData() {
+		
+	}
+	
+	
+	public String getCatalogName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getColumnClassName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getColumnCount() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getColumnDisplaySize(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getColumnLabel(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getColumnName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getColumnType(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getColumnTypeName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPrecision(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getScale(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getSchemaName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTableName(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isAutoIncrement(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isCaseSensitive(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isCurrency(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isDefinitelyWritable(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int isNullable(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isReadOnly(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isSearchable(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isSigned(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isWritable(int column) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isWrapperFor(Class arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object unwrap(Class arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
